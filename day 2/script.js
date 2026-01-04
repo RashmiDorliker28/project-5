e489d2bbc9a5693827c9ac31601cc6b41bc90f90
RashmiDorliker28 <rdorliker@gmail.com>
2026-01-04 21:34:23 +0530

    js code

---
+++ day 2/script.js
@@ -0,0 +1,4 @@
+console.log("This a day 2")
+// double quote is used to print string data type
+console.log("live sever is added")
+console.log()
---
+++ day 3/script.js
@@ -0,0 +1,13 @@
+let abc=1234
+abc=45
+console.log(abc)
+console.log(typeof(abc))
+
+abc="mango"
+console.log(abc)
+console.log(typeof(abc))
+
+let a=false
+console.log(a)
+console.log(typeof(a))
+
